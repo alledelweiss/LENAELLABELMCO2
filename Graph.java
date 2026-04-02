54c5477fbe148f68240720027c1e4c276a9a0bd0
IsabearC <149287854+IsabearC@users.noreply.github.com>
2026-04-02 17:59:54 +0800

    read file and load graph functions

---
+++ Graph.java
@@ -0,0 +1,99 @@
+/*
+ * Graph class reads a social network data file and stores it as a bidirectional graph using an adjacency list. 
+ *
+ * The input file format:
+ *  (1) First line: two integers n and e (number of accounts and number of friendships)
+ *  (2) Following e lines: pairs of integers a b, representing that person a is friends with person b
+ *
+ * After reading the file, the graph is stored as an ArrayList of integers for each person.
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;;
+
+public class Graph {
+    public static ArrayList<Integer>[] graph; // stores the graph in an adjacenty list
+    public static int n; // number of people in the network
+
+    // READ FILE (user inputs filename)
+    public static int[][] readFile() {
+        int[][] edges = null;
+
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Input file path: ");
+            String fileName = input.nextLine();
+
+            File file = new File(fileName);
+            Scanner fileReader = new Scanner(file);
+
+            n = fileReader.nextInt();
+            int e = fileReader.nextInt(); // number of friendships
+
+            edges = new int[e][2];
+
+            // reading each pair of friends
+            for (int i = 0; i < e; i++) {
+                edges[i][0] = fileReader.nextInt(); // person1
+                edges[i][1] = fileReader.nextInt(); // person2
+            }
+
+            fileReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found.");
+            e.printStackTrace();
+        }
+
+        return edges;
+    }
+
+    // BUILD GRAPH
+    public static void loadGraph(int[][] edges){
+        graph = new ArrayList[n];
+
+        // initialization
+        for (int i = 0; i < n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        int numOfEdges = edges.length;
+
+        for (int i = 0; i < numOfEdges; i++) {
+            int edge1 = edges[i][0];
+            int edge2 = edges[i][1];
+
+            // add edge if not already in the list
+            if (!graph[edge1].contains(edge2)) {
+                graph[edge1].add(edge2);
+            }
+            if (!graph[edge2].contains(edge1)) {
+                graph[edge2].add(edge1); 
+            }
+        }
+    }
+
+    // CALL BOTH FUNCTIONS
+    public static void initializeGraph() {
+        int[][] edges = readFile(); 
+
+        if (edges != null) {
+            loadGraph(edges);  
+            System.out.println("Graph loaded!");     
+
+            /***********************************************
+            prints out the adjaceny list for double checking
+            ************************************************
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(i + ": ");
+                for (int friend : graph[i]) {
+                    System.out.print(friend + " ");
+                }
+                System.out.println();
+            }
+            */
+        }
+    }
+}
